9ef5a20e2a5982698fa2d4e19f956e370f0eae70
johnnycodepoland <waciak987@icloud.com>
2026-06-28 23:06:08 +0200

    feat: transaction adding dialog

--- gui/add_transaction_dialog.py
+++ gui/add_transaction_dialog.py
@@ -1,23 +1,81 @@
-from PyQt6.QtWidgets import QDialog, QWidget, QVBoxLayout, QLineEdit
+import datetime
+from PyQt6.QtWidgets import QDialog, QWidget, QVBoxLayout, QLineEdit, QComboBox, QPushButton, QMessageBox
 
 
 class AddTransactionDialog(QDialog):
-    def __init__(self):
-
+    # Dodatkowo przekazujemy obiekt Finance, aby wykorzystać ją potem do zapisania dodanej transakcji w bazie danych
+    def __init__(self, finance):
         # Inicjalizacja klasy nadrzędnej, bez której program nie będzie poprawnie działał
         super().__init__()
 
+        # Inicjalizujemy klasę finance
+        self.finance = finance
+
         # Ustawiamy tytuł okna formularza
-        self.setWindowTitle("Add transaction")
+        self.setWindowTitle("Dodaj transakcje")
 
         # Dodajemy layout
         central_layout = QVBoxLayout(self)
 
         # Tworzymy obiekt QLineEdit, który pozwoli nam wprowadzać tekst
-        amount_input = QLineEdit()
+        self.amount_input = QLineEdit()
 
         # Ustawiamy nazwę dla placeholderu, do przyjmowania kwoty transakcji
-        amount_input.setPlaceholderText("Kwota")
+        self.amount_input.setPlaceholderText("Kwota")
 
         # Dodajemy widget do wprowadzania kwoty
-        central_layout.addWidget(amount_input)
+        central_layout.addWidget(self.amount_input)
+
+        # Tworzymy obiekt QComboBox, który pozwoli nam wybierać elementy z rozwijanej listy
+        self.type_input = QComboBox()
+
+        # Dodajemy opcje wyboru typu transakcji
+        self.type_input.addItem("Przychód")
+
+        self.type_input.addItem("Wydatek")
+
+        # Dodajemy widget do wprowadzania typu transakcji
+        central_layout.addWidget(self.type_input)
+
+        # Tworzymy obiekt QLineEdit, który pozwoli nam wprowadzać tekst
+        self.category_input = QLineEdit()
+
+        # Ustawiamy nazwę dla placeholderu, do przyjmowania kategori transakcji
+        self.category_input.setPlaceholderText("Kategoria")
+
+        # Dodajemy widget do wprowadzania kategorii
+        central_layout.addWidget(self.category_input)
+
+        # Dodajemy przycisk do zatwierdzania formularza
+        confirm_button = QPushButton("Dodaj transakcje")
+
+        # Korzystamy z funkcji clicked, która wykonuje konkretną funkcję po kliknięciu danego przycisku
+        confirm_button.clicked.connect(self.add_transaction)
+
+        # Dodajemy przycisk do zatwierdzania formularza, do głównego layoutu
+        central_layout.addWidget(confirm_button)
+
+    # Tworzymy funkcję, która poprzez klasę Finance zapiszę nam naszą nową transakcję
+    def add_transaction(self):
+        # Wyciągamy z wartość tekstową, z pól formularza, przy pomocy metody text()
+        try:
+            amount = float(self.amount_input.text())
+        except ValueError:
+            QMessageBox.information(self, "Błąd", "Podaj poprawną kwotę transakcji",)
+            return
+        type = self.type_input.currentText()
+        category = self.category_input.text()
+
+        # Zmieniamy język zmiennej "type" na j.ang
+        type = "expense" if type == "Wydatek" else "income"
+
+        # Zamieniamy kwotę transakcji na ujemną, jeśli jest ona wydatkiem
+        if type == "expense":
+            amount = 0 - amount
+
+        # Zapisujemy transakcję korzystając z funkcji klasy Finance
+        self.finance.add_transaction(amount,  str(datetime.date.today()), type, category)
+
+        # Akceptujemy prawidłowe zakończenie, dodawania danych z formularza
+        self.accept()
+
--- gui/main_gui.py
+++ gui/main_gui.py
@@ -1,7 +1,8 @@
 import sys
 from finance import Finance
-from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout, QTabWidget, QPushButton, QHBoxLayout, QLabel, QTableWidget, QHeaderView, QTableWidgetItem
+from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout, QTabWidget, QPushButton, QHBoxLayout, QLabel, QTableWidget, QHeaderView, QTableWidgetItem, QAbstractItemView
 from PyQt6.QtGui import QFont
+from add_transaction_dialog import AddTransactionDialog
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -42,6 +43,9 @@ class MainWindow(QMainWindow):
         # Dodajemy przycisk "Dodaj transakcje"
         transactions_button = QPushButton("Dodaj transakcje")
 
+        # Korzystamy z funkcji clicked, która wykonuje konkretną funkcję po kliknięciu danego przycisku, korzystamy z niej jako referencji, czyli informujemy program o tym, że ma się ona wykonać dopiero wtedy, kiedy zostanie spełniony jakiś warunek
+        transactions_button.clicked.connect(self.open_add_transaction_dialog)
+
         # Dodajemy przycisk "Dodaj transakcje" do layoutu na zakładki
         tab_layout.addWidget(transactions_button)
 
@@ -67,7 +71,7 @@ class MainWindow(QMainWindow):
         tab_layout.addWidget(cyclical_button)
 
         # Dodajemy przycisk "Raporty"
-        report_button = QPushButton("Raporty")
+        report_button = QPushButton("Oszczędności")
 
         # Dodajemy report_button do layoutu na zakładki
         tab_layout.addWidget(report_button)
@@ -106,49 +110,49 @@ class MainWindow(QMainWindow):
         self.content_layout.addWidget(top_widget)
 
         # Dodajemy widget na balans
-        balance_label = QLabel(f"Saldo: {self.finance.return_balance()}zł")
+        self.balance_label = QLabel(f"Saldo: {self.finance.return_balance()}zł")
 
         # Ustawiamy styl widgetu na balans
-        balance_label.setStyleSheet("background-color: white; border-radius: 10px; padding: 10px;")
+        self.balance_label.setStyleSheet("background-color: white; border-radius: 10px; padding: 10px;")
 
         # Ustawiamy wielkość widgetu na balans
-        balance_label.setFixedSize(600, 120)
+        self.balance_label.setFixedSize(600, 120)
 
         # Ustawiamy czcionkę i jej rozmiar
-        balance_label.setFont(QFont("Arial", 70))
+        self.balance_label.setFont(QFont("Arial", 70))
 
         # Dodajemy widget na wpłaty do górnego widgetu na treść
-        top_layout.addWidget(balance_label)
+        top_layout.addWidget(self.balance_label)
 
         # Dodajemy widget na wpłaty
-        income_label = QLabel(f"Wpłaty: {self.finance.return_income()}zł")
+        self.income_label = QLabel(f"Wpłaty: {self.finance.return_income()}zł")
 
         # Ustawiamy styl widgetu na wpłaty
-        income_label.setStyleSheet("background-color: white; border-radius: 10px; padding: 10px;")
+        self.income_label.setStyleSheet("background-color: white; border-radius: 10px; padding: 10px;")
 
         # Ustawiamy wielkość widgetu na wpłaty
-        income_label.setFixedSize(400, 120)
+        self.income_label.setFixedSize(400, 120)
 
         # Ustawiamy czcionkę i jej rozmiar
-        income_label.setFont(QFont("Arial", 40))
+        self.income_label.setFont(QFont("Arial", 40))
 
         # Dodajemy widget na wpłaty do górnego widgetu na treść
-        top_layout.addWidget(income_label)
+        top_layout.addWidget(self.income_label)
 
         # Dodajemy widget na wydatki
-        expense_label = QLabel(f"Wydatki: {self.finance.return_expense()}zł")
+        self.expense_label = QLabel(f"Wydatki: {self.finance.return_expense()}zł")
 
         # Ustawiamy styl widgetu na wydatki
-        expense_label.setStyleSheet("background-color: white; border-radius: 10px; padding: 10px;")
+        self.expense_label.setStyleSheet("background-color: white; border-radius: 10px; padding: 10px;")
 
         # Ustawiamy wielkość widgetu na wydatki
-        expense_label.setFixedSize(400, 120)
+        self.expense_label.setFixedSize(400, 120)
 
         # Ustawiamy czcionkę i jej rozmiar
-        expense_label.setFont(QFont("Arial", 40))
+        self.expense_label.setFont(QFont("Arial", 40))
 
         # Dodajemy widget na wydatki do górnego widgetu na treść
-        top_layout.addWidget(expense_label)
+        top_layout.addWidget(self.expense_label)
 
         # Wypychamy widget na saldo na maksa w lewo
         top_layout.addStretch()
@@ -180,36 +184,76 @@ class MainWindow(QMainWindow):
         table_container.setStyleSheet("#table_container {background-color: white; border-radius: 10px; padding: 10px;}")
 
         # Dodajemy widget na historię transakcji
-        history_table = QTableWidget(0, 4)
+        self.history_table = QTableWidget(0, 4)
+
+        # Blokujemy możliwość edycji komórek w tabeli
+        self.history_table.setEditTriggers(QAbstractItemView.EditTrigger.NoEditTriggers)
 
         # Importujemy wszystkie transakcje, korzystając z funkcji show_history()
         transactions = self.finance.show_history()
 
         # Iterujemy przez wszystkie transakcje, aby dodać je do tabeli z ostatnimi transakcjami
         for transaction in transactions:
-            row = history_table.rowCount()
-            history_table.insertRow(row)
+            row = self.history_table.rowCount()
+            self.history_table.insertRow(row)
             amount = abs(transaction[1])
-            history_table.setItem(row, 0, QTableWidgetItem(str(amount)))
-            history_table.setItem(row, 1, QTableWidgetItem(str(transaction[2])))
+            self.history_table.setItem(row, 0, QTableWidgetItem(str(amount)))
+            self.history_table.setItem(row, 1, QTableWidgetItem(str(transaction[2])))
             type = "wydatek" if transaction[3] == "expense" else "przychód"
-            history_table.setItem(row, 2, QTableWidgetItem(type))
-            history_table.setItem(row, 3, QTableWidgetItem(str(transaction[4])))
+            self.history_table.setItem(row, 2, QTableWidgetItem(type))
+            self.history_table.setItem(row, 3, QTableWidgetItem(str(transaction[4])))
 
         # Ustawiamy automatyczne wypełnianie całej dostępnej przestrzeni przez kolumny
-        history_table.horizontalHeader().setSectionResizeMode(QHeaderView.ResizeMode.Stretch)
+        self.history_table.horizontalHeader().setSectionResizeMode(QHeaderView.ResizeMode.Stretch)
 
         # Dodajemy widget na historię transakcji do layoutu "kontenera" na transakcje
-        table_layout.addWidget(history_table)
+        table_layout.addWidget(self.history_table)
 
         # Ustawiamy nagłówki / nazwy kolumn dla tabeli history_table
-        history_table.setHorizontalHeaderLabels(["Kwota", "Data", "Typ", "Kategoria"])
+        self.history_table.setHorizontalHeaderLabels(["Kwota", "Data", "Typ", "Kategoria"])
 
         # Dodajemy "kontener" na transakcje, do dolnego layoutu
         bottom_layout.addWidget(table_container)
 
         # Wypychamy dolną część treści na maksa w lewo
         bottom_layout.addStretch()
+
+    # Ta funkcji pozwoli nam uniknąć otwierania okna dodawania transakcji za każdym razem po włączeniu programu
+    def open_add_transaction_dialog(self):
+        # Inicjalizujemy klasę AddTransactionDialog
+        dialog = AddTransactionDialog(self.finance)
+
+        # Korzystamy z metody QDialog exec(), która pozwoli nam wyświetlić formularz dodawania transakcji, blokująć przy tym korzystanie z wszytkich innych okien aplikacji
+        dialog.exec()
+
+        # Korzystamy z funkcji refresh_dashboard, do odświeżenia dashboardu
+        self.refresh_dashboard()
+
+    # Tworzymy funkcje do odświeżania dashboardu
+    def refresh_dashboard(self):
+        # Ustawiamy na nowo wszystkie wartości widgetów
+        self.balance_label.setText(f"Saldo: {self.finance.return_balance()}zł")
+
+        self.income_label.setText(f"Wpłaty: {self.finance.return_income()}zł")
+
+        self.expense_label.setText(f"Wydatki: {self.finance.return_expense()}zł")
+
+        # Resetujemy liczbę wierszy
+        self.history_table.setRowCount(0)
+
+        # Importujemy wszystkie transakcje, korzystając z funkcji show_history()
+        transactions = self.finance.show_history()
+
+        # Iterujemy przez wszystkie transakcje, aby dodać je do tabeli z ostatnimi transakcjami
+        for transaction in transactions:
+            row = self.history_table.rowCount()
+            self.history_table.insertRow(row)
+            amount = abs(transaction[1])
+            self.history_table.setItem(row, 0, QTableWidgetItem(str(amount)))
+            self.history_table.setItem(row, 1, QTableWidgetItem(str(transaction[2])))
+            type = "wydatek" if transaction[3] == "expense" else "przychód"
+            self.history_table.setItem(row, 2, QTableWidgetItem(type))
+            self.history_table.setItem(row, 3, QTableWidgetItem(str(transaction[4])))
 
 app = QApplication(sys.argv)
 window = MainWindow()
